dbfadc8a854500c0c02c3d690f36216e82ca1c48
Gama <elikosamed@gmail.com>
2025-11-02 14:45:42 +0400

    new

--- news_portal/settings.py
+++ news_portal/settings.py
@@ -42,6 +42,10 @@ INSTALLED_APPS = [
     'cloudinary_storage',
     'accounts',
 ]
+import os
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
